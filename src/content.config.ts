552c3cdd357d79fa16e739af37d7b98c7e14ea71
Cristian Fernandez Del Pozo <cristian.fernandez.del.pozo@gmail.com>
2026-04-25 11:20:25 +0200

    feat: fix lint issues

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,5 +1,6 @@
-import { defineCollection, z } from "astro:content";
+import { defineCollection } from "astro:content";
 import { glob } from "astro/loaders";
+import { z } from "zod";
 import { CATEGORY_VALUES, DEFAULT_CATEGORY } from "./data/categories";
 
 const blog = defineCollection({
